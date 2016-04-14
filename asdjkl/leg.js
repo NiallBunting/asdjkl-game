5bdcbdafd714b9751ea53ffcb27fd0127931d5bf
NiallBunting <niallbunting@googlemail.com>
2016-04-15 00:02:14 +0100

    Updated the leg creation to infer more
    Changed how the legs work, now works more as a linked list.

--- asdjkl/leg.js
+++ asdjkl/leg.js
@@ -1,81 +1,105 @@
 var Leg = {
 
 	p_parent: null,
+	p_child: null,
 	p_pos: 0,
 	p_x: 0,
 	p_y: 0,
 	p_length: 100,
 	p_angle: 0,
 
-	create: function(parent, pos, x, y, length, angle){
+	create: function(parent, length, angle){
 		var obj = Object.create(this);
-		obj.init(parent, pos, x, y, length, angle);
+		obj.init(parent, length, angle);
 		return obj;
 	},
 
-	init: function(parent, pos, x, y, length, angle){
+	init: function(parent, length, angle){
 		this.p_parent = parent;
-		this.p_pos = pos;
-		this.p_x = x;
-		this.p_y = y;
+		this.p_x = parent.getEndX();
+		this.p_y = parent.getEndY();
 		this.p_length = length;
 		this.p_angle = angle;
+		this.p_pos = 0;
+	},
+
+	addChild: function(length, angle){
+		var parent = this;
+		var pos = this.p_pos + 1;
+		this.p_child = Leg.create(parent, length, angle);
+		this.p_child.setPos();
+		return this.p_child;
 	},
 
 	update: function(){
-		if(this.p_pos < 1){
-		this.p_angle = this.p_angle + ((Math.random() - 0.5) * 0.1);
+		if(!this.checkContact()){
+			this.updateX();
+			this.updateY();
+		}
+		if(this.p_child != null){
+			this.p_child.update();
+		}
+	},
+
+	checkContact: function(){
+		if(this.p_y < 5 || this.getEndY() < 5){
+			//NEED TO DO SOMETHING HERE THAT
+			//CAUSES THE REST OF THE LEG TO PHYSIC
+			if(this.p_child != null){this.p_child.moveToContact(false);}
+			if(this.p_parent != null){this.p_parent.moveToContact(true);}
+			return true;
+		}
+		else {return false;}
+	},
+
+	moveToContact: function(parentBool){
+		if(parentBool){
+			//parent of thing
+			this.p_angle = Math.atan2(this.p_child.p_x, this.p_child.p_y);
+			if(this.p_parent != null){this.p_parent.moveToContact(true);}
+		}else{
+			//child of thing
+			
 		}
 	},
 
 	draw: function(canvas, ctx){
 		ctx.lineWidth = 3;
 		ctx.beginPath();
-		ctx.moveTo(this.getX(), this.getY());
+		ctx.moveTo(this.p_x, this.p_y);
 		ctx.lineTo(this.getEndX(), this.getEndY());
 		ctx.strokeStyle = "red";
 		ctx.stroke();
+		if(this.p_child != null){
+			this.p_child.draw(canvas, ctx);
+		}
 	},
 
-	getX: function(){
-		var parentLeg = this.p_parent;
-
-		if(parentLeg != null){
-			return this.p_parent.getEndX();
-		}
-		return this.p_x;
+	updateX: function(){
+		return this.p_x = this.p_parent.getEndX();
 
 	},
 
-	getY: function(){
-		var parentLeg = this.p_parent;
+	updateY: function(){
+		return this.p_y = this.p_parent.getEndY();
+	},
 
-		if(parentLeg != null){
-			return this.p_parent.getEndY();
+	parentAngle: function(){
+		var parentLeg = this.p_parent;
+		var angle = 0.0;
+		while(parentLeg != null){
+		      angle = angle + parentLeg.getAngle();
+		      parentLeg = parentLeg.getParent();
 		}
-		return this.p_y;
+		return angle;
 	},
 
 	getEndX: function(){
-		var parentLeg = this.p_parent;
-		var angle = this.p_angle;
-		while(parentLeg != null){
-		      angle = angle + parentLeg.getAngle();
-		      parentLeg = parentLeg.getParent();
-		}
-
-		return this.getX() + (Math.cos(angle) * this.p_length);
+		return this.p_x + (Math.cos(this.parentAngle() + this.p_angle) * this.p_length);
 	},
 
 	getEndY: function(){
-		var parentLeg = this.p_parent;
-		var angle = this.p_angle;
-		while(parentLeg != null){
-		      angle = angle + parentLeg.getAngle();
-		      parentLeg = parentLeg.getParent();
-		}
-
-		return this.getY() + (Math.sin(angle) * this.p_length);
+		return this.p_y + (Math.sin(this.parentAngle() + this.p_angle) * this.p_length);
 	},
 
 	getAngle: function(){
@@ -84,5 +108,13 @@ var Leg = {
 
 	getParent: function(){
 		return this.p_parent;
+	},
+
+	getPos: function(){
+		return this.p_pos;
+	},
+
+	setPos: function(){
+		this.p_pos = this.p_parent.getPos() + 1;
 	}
 };
--- asdjkl/player.js
+++ asdjkl/player.js
@@ -1,8 +1,8 @@
 var Player = {
 
 	p_legs: null,
-	p_x: 0,
-	p_y: 0,
+	p_x: 200,
+	p_y: 200,
 
 	create: function(){
 		var obj = Object.create(this);
@@ -11,38 +11,45 @@ var Player = {
 	},
 
 	init: function(){
-		this.p_legs = [
-				[[10], [11], [12]],
-				[[20], [21], [22]],
-				[[30], [31], [32]]
-			];
+		this.p_legs = [[0], [1], [2]];
 
 		for(var i = 0; i < 3; i++){
-			for(var j = 0; j < 3; j++){
-				var parent = null;
-				if(j > 0){
-					parent = this.p_legs[i][j - 1];
-				}
-				var length = 50;
-				if(j == 2){length = 10};
-				this.p_legs[i][j] = Leg.create(parent, j, 200, 200, length, Math.random());
-			}
+			this.p_legs[i] = Leg.create(this, 50, i * 2);
+			var thigh = this.p_legs[i].addChild(50, 0.4);
+			var foot = thigh.addChild(10, 0.5);
 		}
 	},
 
 	update: function(){
+		this.p_y = this.p_y - 0.5;
 		for(var i = 0; i < 3; i++){
-			for(var j = 0; j < 3; j++){
-				this.p_legs[i][j].update();
-			}
+			this.p_legs[i].update();
 		}
 	},
 
 	draw: function(canvas, ctx){
 		for(var i = 0; i < 3; i++){
-			for(var j = 0; j < 3; j++){
-				this.p_legs[i][j].draw(canvas, ctx);
-			}
+			this.p_legs[i].draw(canvas, ctx);
 		}
+	},
+
+	getEndX: function(){
+		return this.p_x;
+	},
+
+	getEndY: function(){
+		return this.p_y;
+	},
+
+	getAngle: function(){
+		return 0;
+	},
+
+	getParent: function(){
+		return null;
+	},
+
+	moveToContact: function(){
+		return null;
 	}
 };
