2aeea38e27ab2d41e6dc5bea8dab57c1ca844e4f
NiallBunting <niallbunting@googlemail.com>
2016-04-19 23:42:39 +0100

    Starting to get one leg working
    - Added some colour to make it easier to see which node is which.

--- asdjkl/hinge.js
+++ asdjkl/hinge.js
@@ -19,31 +19,56 @@ var Hinge = {
 
 	update: function(){
 		if(this.p_child != null){
-			var childAngle = Math.atan2(this.p_child.getX(), this.p_child.getY());
-			if(childAngle < this.p_angleMin){
-				childAngle = this.p_angleMin;
-			}
-
-			if(childAngle > this.p_angleMax){
-				childAngle = this.p_angleMax;
-			}
+			var childAngle = Math.atan2(this.p_child.getY(), this.p_child.getX()) - Math.atan2(this.p_y, this.p_x);
+			if (childAngle < 0){ childAngle += 2 * Math.PI; }
+			//if(childAngle < this.p_angleMin){
+				//childAngle = this.p_angleMin;
+			//}
+//
+			//if(childAngle > this.p_angleMax){
+				//childAngle = this.p_angleMax;
+			//}
 
 			this.p_child.p_x = this.getEndX(childAngle, this.p_length);
 			this.p_child.p_y = this.getEndY(childAngle, this.p_length);
 			this.p_child.update();
 
-			var dist = Math.sqrt(Math.pow((this.p_x - this.p_child.p_x), 2) + Math.pow((this.p_y - this.p_child.p_y), 2));
-			
-			if(dist > 1){
-				//need to work out negative dist
-				var multiplier = this.p_length / dist;
 
-				this.p_x = this.p_x * multiplier;
-				this.p_y = this.p_y * multiplier;
+			var Angle = Math.atan2(this.p_y, this.p_x) - Math.atan2(this.p_child.getY(), this.p_child.getX());
+			if (Angle < 0){ Angle += 2 * Math.PI; }
+			
+			var dist = Math.sqrt(Math.pow((this.p_x - this.p_child.p_x), 2) + Math.pow((this.p_y - this.p_child.p_y), 2));
+
+			if(this.length + 1 > dist || this.length - 1 < dist){
+				this.p_x = this.p_child.getX() + (Math.cos(Angle) * this.p_length);
+				this.p_y = this.p_child.getY() + (Math.sin(Angle) * this.p_length);
 			}
+
+		//	
+			//var multiplier = this.p_length / dist;
+			//if(this.p_x > this.p_child.p_x){
+				//this.p_x = this.p_x * multiplier;
+			//}else{
+				//this.p_x = (-this.p_x) * multiplier;
+			//}
+			//if(this.p_y > this.p_child.p_y){
+				//this.p_y = this.p_y * multiplier;
+			//}else{
+				//this.p_y = (-this.p_y) * multiplier;
+			//}
 		}
 		else{
-			this.p_y -= 0.1;
+			if(this.p_x < 200){
+				this.p_x += 50;
+			}else{
+				this.p_x -= 50;
+			}
+			if(this.p_y < 200){
+				this.p_y += 50;
+			}else{
+				this.p_y -= 50;
+			}
+			console.log(this.p_x + " | " + this.p_y);
 		}
 	},
 
@@ -57,7 +82,14 @@ var Hinge = {
 			ctx.strokeStyle = "red";
 			ctx.stroke();
 
+			ctx.rect(this.p_x - 10,this.p_y - 10,20,20);
+			ctx.stroke();
+
 			this.p_child.draw(canvas, ctx);
+		}else{
+			ctx.fillStyle = "blue";
+			ctx.rect(this.p_x - 5,this.p_y - 5,10,10);
+			ctx.fill();
 		}
 	},
 
